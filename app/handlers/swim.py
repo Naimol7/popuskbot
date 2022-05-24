d9d464383dd2ba69b5ad5b972f26e24fa860ffb8
Tim <tima.ivanov00@gmail.com>
2022-05-24 20:27:33 +0300

    Спасибо Тимофей

--- app/handlers/swim.py
+++ app/handlers/swim.py
@@ -8,6 +8,8 @@ import os
 import requests
 import json
 
+IP = '192.168.1.120'
+
 menu_kb = ReplyKeyboardMarkup(resize_keyboard=True)
 v1 = KeyboardButton(text='М')
 v2 = KeyboardButton(text='Ж')
@@ -40,7 +42,7 @@ async def sex(message: types.Message, state: FSMContext):
         await message.answer("Вы ввели что-то неправильно", reply_markup=menu_kb)
     else:
         await state.update_data(sex=message.text)
-        r = requests.get(url='http://192.168.1.120:5000/getstyles')
+        r = requests.get(url=f'http://{IP}:5000/getstyles')
         styles_list = json.loads(r.text)
         print([i['name'] for i in styles_list])
         menu_kb2.add(*[i['name'] for i in styles_list] )
@@ -52,33 +54,23 @@ async def style(message: types.Message, state: FSMContext):
         await message.answer("Вы ввели что-то неправильно", reply_markup=menu_kb)
     else:
         await state.update_data(style=message.text)
-        await message.answer('Введите дистанцию (м)')
-        if message.text == 'Вольный':
-            await SwimmingStates.waitingDistance1.set()
-        if message.text == 'Спина':
-            await SwimmingStates.waitingDistance2.set()
-        if message.text == 'Брасс':
-            await SwimmingStates.waitingDistance3.set()
-        if message.text == 'Баттерфляй':
-            await SwimmingStates.waitingDistance4.set()
-        if message.text == 'Комплекс':
-            await SwimmingStates.waitingDistance5.set()
-
-async def distance1(message: types.Message, state: FSMContext):
-
+        r = requests.get(url=f'http://{IP}:5000/getdistances/{message.text}')
+        distances_list = json.loads(r.text)
+        print([i['length'] for i in distances_list])
+        kb = ReplyKeyboardMarkup(resize_keyboard=True)
+        kb.add(*[str(i['length']) for i in distances_list] )
+        await message.answer('Введите дистанцию',reply_markup=kb)
+        await SwimmingStates.waitingDistance.set()
+        
+async def distance(message: types.Message, state: FSMContext):
     await state.update_data(distance=message.text)
-    await message.answer('Введите время (мм.сс,мсмс)')
+    await message.answer('введите время')
     await SwimmingStates.waitingTime.set()
 
 async def time(message: types.Message, state: FSMContext):
     await state.update_data(time=message.text)
     user_data = await state.get_data()
-    # await message.answer(user_data)
-    # array = list(user_data.values())
-    sex = user_data['sex']
-    style = user_data['style']
-    distance = user_data['distance']
-    # time = user_data['time']
+    print(user_data)
     await message.answer(sex, style, distance)
     
 
@@ -86,5 +78,5 @@ def register_by_swimming_handlers(dp: Dispatcher):
     dp.register_message_handler(start, commands='start', state='*')
     dp.register_message_handler(sex, state=SwimmingStates.waitingSex)
     dp.register_message_handler(style, state=SwimmingStates.waitingStyle)
-    # dp.register_message_handler(distance, state=SwimmingStates.waitingDistance)
+    dp.register_message_handler(distance, state=SwimmingStates.waitingDistance)
     dp.register_message_handler(time, state=SwimmingStates.waitingTime)
